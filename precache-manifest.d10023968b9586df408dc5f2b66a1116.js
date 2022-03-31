518125b186708379afd2b4bc9ae797ef60450270
Pijamo <ngulliejake@gmail.com>
2022-04-01 00:06:50 +0530

    Updates

---
+++ precache-manifest.d10023968b9586df408dc5f2b66a1116.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e14ee0c864bedec1fbc7dca0bbcfda8",
+    "url": "/SeaQuill/index.html"
+  },
+  {
+    "revision": "7922f000f23720914ed5",
+    "url": "/SeaQuill/static/js/2.eba42a2c.chunk.js"
+  },
+  {
+    "revision": "5024038dad139e59dbda0dca8a3194f4",
+    "url": "/SeaQuill/static/js/2.eba42a2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb58c4653029d658a06d",
+    "url": "/SeaQuill/static/js/main.255881ef.chunk.js"
+  },
+  {
+    "revision": "6f552b99601bef57315c",
+    "url": "/SeaQuill/static/js/runtime-main.858c1082.js"
+  }
+]);
