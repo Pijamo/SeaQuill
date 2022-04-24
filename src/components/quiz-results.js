b46d34c97703eb9b61ed7422ecf9cc832f17b889
Alex Vena <alexvena@seas.upenn.edu>
2022-04-23 23:24:44 -0600

    Added route 2 and tested in quiz-results

--- src/components/quiz-results.js
+++ src/components/quiz-results.js
@@ -7,31 +7,53 @@ import CallToActionV1 from './section-components/call-to-action-v1';
 import Footer from './global-components/footer';
 import { useLocation } from 'react-router-dom';
 
-import {getCounties} from '../fetcher' 
+import {getCounties, getCities} from '../fetcher' 
 
 import ResultsTable from './quiz-results-components/quizResultsTable'
 
 
 export default function QuizResults(){
-    let [results, setResults] = useState()
-    let page = 0
-    let pagesize = 0
-    let zip = 0
+
+    // Counties
+    let [counties, setCounties] = useState()
+    let [countyPage, setCountyPage] = useState(0)
+    let [countyPagesize, setCountyPagesize ] = useState(0)
+    let [zip, setZip] = useState(0)
+
     const location = useLocation()
+    
     // const ratings = Object.values(location.state.ratingData)
-    const ratings = Array(11).fill(5)
+    const ratings = Array(11).fill(5) // HARD CODED VALUES (for quicker testing)
     useEffect(()=> {
         // page, pagesize, userzip, 11 prosperity ratings 
-        getCounties(page,pagesize,zip, ...ratings) 
-        .then(data=>setResults(data["results"]))
+        getCounties(countyPage,countyPagesize,zip, ...ratings) 
+        .then(data=>setCounties(data["results"]))
     }, [])
     
-    function showResults(results){
-        let CountyList = results.map((item,index)=>{
+    function showCounties(results){
+        let CountiesList = results.map((item,index)=>{
             return <div>{item.county}, {item.state}, {item.fips_code} - {item.total_score.toFixed(2)}</div>
 
         })
-        return CountyList
+        return CountiesList
+    }
+
+
+     //Retrieve cities for a particular county
+     let [cities, setCities] = useState()
+
+     useEffect(()=> {
+        // page, pagesize, userzip, 11 prosperity ratings 
+        getCities(1, 10, 0, 10000000, 6037) 
+        .then(data=>setCities(data["results"]))
+    }, [])
+
+    function showCities(results){
+        let CitiesList = results.map((item,index)=>{
+            return <div>{item.city}, {item.Population}, {item.Max_Population_Density.toFixed(0)} </div>
+
+        })
+        return CitiesList
     }
 
     return (
@@ -40,8 +62,8 @@ export default function QuizResults(){
             <ResultsTable />
             {/* <PageHeader headertitle="Quiz Results" /> */}
         
-                {/* {results && 
-                    showResults(results)} */}
+                {/* {counties && showCounties(counties)} */}
+                {/* {cities && showCities(cities)} */}
             <Footer />
         </div>  
     )
