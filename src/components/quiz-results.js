70d3b71d23e56c274adca6f665c4ef1a526412d8
Alex Vena <alexvena@seas.upenn.edu>
2022-04-26 17:40:39 -0600

    Manual merge of Pijamo's additions

--- src/components/quiz-results.js
+++ src/components/quiz-results.js
@@ -1,29 +1,61 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext, useState, useEffect, useCallback } from 'react';
 
 import Navbar from './global-components/navbar';
-import PageHeader from './global-components/page-header';
+import { Modal, Button } from 'react-bootstrap';
 
 import Footer from './global-components/footer';
-import { useLocation } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 
-import {getCounties, getScores, getCities, getClimate, getJobs} from '../fetcher' 
-import Climate from './climate'
-import Jobs from './job-table-v1'
-import Prosperity from './prosperity';
-import ResultsTable from './quiz-results-components/quizResultsTable'
+import {getCounties, getCities, getClimate, getJobs} from '../fetcher' 
+import Climate from '../components/climate'
+import Jobs from '../components/job-table-v1'
 
+import BootstrapTable from 'react-bootstrap-table-next';
+import paginationFactory from 'react-bootstrap-table2-paginator';
 
 
 export default function QuizResults(){
 
     // Counties
-    let [counties, setCounties] = useState()
-    let [countyPage, setCountyPage] = useState(null)
-    let [countyPagesize, setCountyPagesize ] = useState(null)
+    let [counties, setCounties] = useState([])
+    let [countyPage, setCountyPage] = useState(1)
+    let [countyPagesize, setCountyPagesize ] = useState(20)
     let [zip, setZip] = useState(0)
 
+    //Retrieve cities for a particular county
+     let [cities, setCities] = useState([])
+     let [popLower, setPopLower] = useState(null)
+     let [popUpper, setPopUpper ] = useState(null)
+     let [cityPage, setCityPage] = useState(null)
+     let [cityPagesize, setCityPagesize ] = useState(null)
+
+     //Retrieve climate information for the selected county
+     let [climate, setClimate] = useState([])
+
+     //Retrieve cities for a particular county
+     let [jobs, setJobs] = useState([])
+     let [jobTable, setJobTable] = useState()
+     let [jobTitle, setJobTitle] = useState()
+     let [jobPage, setJobPage] = useState(null)
+     let [jobPagesize, setJobPagesize ] = useState(null)
+
+     //modal
+     const [show, setShow] = useState(false);
+     const handleClose = () => setShow(false);
+    //  const handleShow = () => {
+    //      setShow(true);
+    //     };
+    const handleShow = useCallback(
+        (fips_code) => () => {
+            testClimate(fips_code);
+            testJobs(fips_code);
+            setShow(true);
+        },
+        [],
+    )
+
     const location = useLocation()
-    
+
     // let ratings = location.state.ratingData
     // let environment = isNaN(ratings.environment) ? 1 : ratings.environment
     // let business = isNaN(ratings.business) ? 1 : ratings.business
@@ -38,12 +70,30 @@ export default function QuizResults(){
     // let living = isNaN(ratings.living) ? 1 : ratings.living
   
     const ratingsHard = Array(11).fill(5) // HARD CODED VALUES (for quicker testing)
+
+    
+    
     useEffect(()=> {
         // page, pagesize, userzip, 11 prosperity ratings 
-        getCounties(countyPage,countyPagesize,zip,...ratingsHard) //education,freedom,safety,social,business,economic,infrastructure,governance,health,living,environment
-        .then(data=>setCounties(data["results"]))
+        getCounties(countyPage, countyPagesize, zip, ...ratingsHard) 
+        .then(data => setCounties(data["results"]))
     }, [])
-    
+
+    function testCities(countyID) {
+        getCities(cityPage, cityPagesize, popLower, popUpper, countyID)
+        .then(data=>setCities(data["results"]))
+    }
+
+    function testClimate(countyID) {
+        getClimate(countyID)
+        .then(data=>setClimate(data["results"]))
+    }
+
+    function testJobs(countyId) {
+        getJobs(jobPage, jobPagesize, "chem", countyId)
+        .then(data=>setJobs(data["results"])) 
+    }
+
     function showCounties(results){
         let CountiesList = results.map((item,index)=>{
             return <div>{item.county}, {item.state}, {item.fips_code} - {item.total_score.toFixed(2)}</div>
@@ -52,35 +102,18 @@ export default function QuizResults(){
         return CountiesList
     }
 
-     //Retrieve cities for a particular county
-     let [cities, setCities] = useState()
-     let [popLower, setPopLower] = useState(null)
-     let [popUpper, setPopUpper ] = useState(null)
-     let [cityPage, setCityPage] = useState(null)
-     let [cityPagesize, setCityPagesize ] = useState(null)
-     
-
-     useEffect(()=> {
-        getCities(cityPage, cityPagesize, popLower, popUpper, 6037) // PLACEHOLDER for county id
-        .then(data=>setCities(data["results"]))
-    }, [])
-
     function showCities(results){
         let CitiesList = results.map((item,index)=>{
-            return <div>{item.city}, {item.Population}, {item.Max_Population_Density.toFixed(0)} </div>
-
+            return (
+                <div className="card p-2 m-2" style={{width: "18%"}}>
+                    <Link to = "/listings">
+                        <h5 className="card-title align-self-center">{item.city}</h5>
+                    </Link>
+                </div>
+            )
         })
         return CitiesList
     }
-
-     //Retrieve climate information for the selected county
-     let [climate, setClimate] = useState()
-     
-
-     useEffect(()=> {
-        getClimate(8035) // PLACEHOLDER for county id (6077, 8035, 48001 )
-        .then(data=>setClimate(data["results"]))
-    }, [])
 
     function showClimate(results){
         let ClimateList = results.map((item,index)=>{
@@ -90,42 +123,134 @@ export default function QuizResults(){
         })
         return ClimateList
     }
-
-     //Retrieve cities for a particular county
-     let [jobs, setJobs] = useState()
-     let [jobTable, setJobTable] = useState()
-     let [jobTitle, setJobTitle] = useState()
-     let [jobPage, setJobPage] = useState(null)
-     let [jobPagesize, setJobPagesize ] = useState(null)
-     
-
-     useEffect(()=> {
-        getJobs(jobPage, jobPagesize, "d", 6037) // PLACEHOLDER for keyword, county id
-        .then(data=>setJobs(data["results"]))  
-    }, [])
-
-        //Retrieve prosperity score for a given county
-        let [scores, setScores] = useState()
     
-        useEffect(()=> {
-        getScores(31171) // PLACEHOLDER for keyword, county id
-        .then(data=>setScores(data["results"]))  
-    }, [])
 
+    const expandRow = {
+        
+        renderer: (row) => (
+                <div>
+                    <p>{ `This Expand row is belong to rowKey ${row.county}` }</p>
+                    
+                    {/* {jobs && Jobs(jobs)}
+                    <br/>
+
+                    {climate && Climate(climate)}
+                    <br/> */}
+                    <div className = "d-flex flex-row flex-wrap">
+                        {cities && showCities(cities)}
+                    </div>
+
+                    
+                    <br/>
+                </div>
+            ),
+            showExpandColumn: true,
+            expandByColumnOnly: true,
+            
+            //Header Expansion Sign
+            expandHeaderColumnRenderer: ({ isAnyExpands }) => {
+                if (isAnyExpands) {
+                  return <b></b>;
+                }
+                return <b></b>;
+              },
+            
+            //Column Expansion Custom Sign
+            expandColumnRenderer: ({ expanded }) => {
+            if (expanded) {
+                return (
+                <b>-</b>
+                );
+            }
+            return (
+                <b>...</b>
+            )
+            },
+
+            onExpand: (row, isExpand, rowIndex, e) => {
+                if (isExpand) {
+                    console.log(row.fips_code)
+                    // testJobs(row.fips_code)
+                    // testClimate(row.fips_code)
+                    testCities(row.fips_code)
+                    console.log(jobs)
+                }
+            },
+
+            onlyOneExpanding: true,
+    }
+
+    const scoreFormatter = (data, row ) => {
+        return <>
+        {data.toFixed(2)}
+        </>
+    }
+
+    const columns = [
+        {
+            dataField: "county",
+            text: "County",
+            sort: true,
+        },
+        {
+            dataField: "state",
+            text: "State",
+            sort: true,
+        },
+        {
+            dataField: "total_score",
+            text: "Score",
+            sort: true,
+            formatter: scoreFormatter,
+        },
+        {
+            dataField: "df1",
+            isDummyField: true,
+            text: 'Action 1',
+            formatter: (cellContent, row) => {
+                return (
+                    <>
+                        <Button variant="primary" size="sm" onClick={handleShow(row.fips_code)}> 
+                            Show Details 
+                        </Button>
+                    </>
+                )
+            }
+        }
+    ];
 
     return (
         <div>
-            <Navbar></Navbar>
-            {/* <ResultsTable /> */}
-            {/* <PageHeader headertitle="Quiz Results" /> */}
-        
-                {counties && showCounties(counties)}
-                {/* {cities && showCities(cities)} */}
-                 {jobs && Jobs(jobs)}
-                {climate && Climate(climate)} 
-                {scores && Prosperity(scores)}
+            <Navbar />
+            <div className='w-75 m-auto'>
+                <BootstrapTable 
+                keyField='fips_code' 
+                columns={ columns } 
+                data={ counties } 
+                hover 
+                condensed
+                expandRow={ expandRow }
+                pagination={paginationFactory()}
+                />
+            </div>   
+
+            <Modal show={show} onHide={handleClose} dialogClassName="modal-90w">
+                <Modal.Header closeButton>
+                <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body>
+                    {jobs && Jobs(jobs)}
+                    {climate && Climate(climate)}
+                </Modal.Body>
+                
+                <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose} size="sm">
+                    Close
+                </Button>
+                </Modal.Footer>
+            </Modal>
             <Footer />
         </div>  
     )
-    
 }
