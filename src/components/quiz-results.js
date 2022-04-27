34a92b107e46920e4f8da97f5274e23c052ec0f3
Alex Vena <alexvena@seas.upenn.edu>
2022-04-27 09:48:13 -0600

    Added form for job keyword (need to pass value and re-render table)

--- src/components/quiz-results.js
+++ src/components/quiz-results.js
@@ -36,7 +36,14 @@ export default function QuizResults(){
      //Retrieve jobs for a particular county
      let [jobs, setJobs] = useState([])
      let [jobPage, setJobPage] = useState(null)
+     let [keyword, setKeyword] = useState('data')
      let [jobPagesize, setJobPagesize ] = useState(null)
+
+     const KeywordChangeHandler = (event) => {
+        setKeyword(event.target.value);
+        console.log(event.target.value)
+      };
+
 
      //modal
      const [show, setShow] = useState(false);
@@ -45,6 +52,7 @@ export default function QuizResults(){
     //      setShow(true);
     //     };
     const handleShow = useCallback(
+        
         (fips_code) => () => {
             testClimate(fips_code);
             testJobs(fips_code);
@@ -88,10 +96,9 @@ export default function QuizResults(){
     }
 
     function testJobs(countyId) {
-        getJobs(jobPage, jobPagesize, "che", countyId)
+        getJobs(jobPage, jobPagesize, keyword, countyId)
         .then(data=>setJobs(data["results"])) 
     }
-
 
     function showCities(results){
         let CitiesList = results.map((item,index)=>{
@@ -206,58 +213,7 @@ export default function QuizResults(){
             <Navbar />
             <PageHeader headertitle="Quiz Results"/>
             
-            <Form>
-  <Row className="mb-3">
-    <Form.Group as={Col} controlId="formGridEmail">
-      <Form.Label>Email</Form.Label>
-      <Form.Control type="email" placeholder="Enter email" />
-    </Form.Group>
-
-    <Form.Group as={Col} controlId="formGridPassword">
-      <Form.Label>Password</Form.Label>
-      <Form.Control type="password" placeholder="Password" />
-    </Form.Group>
-  </Row>
-
-  <Form.Group className="mb-3" controlId="formGridAddress1">
-    <Form.Label>Address</Form.Label>
-    <Form.Control placeholder="1234 Main St" />
-  </Form.Group>
-
-  <Form.Group className="mb-3" controlId="formGridAddress2">
-    <Form.Label>Address 2</Form.Label>
-    <Form.Control placeholder="Apartment, studio, or floor" />
-  </Form.Group>
-
-  <Row className="mb-3">
-    <Form.Group as={Col} controlId="formGridCity">
-      <Form.Label>City</Form.Label>
-      <Form.Control />
-    </Form.Group>
-
-    <Form.Group as={Col} controlId="formGridState">
-      <Form.Label>State</Form.Label>
-      <Form.Select defaultValue="Choose...">
-        <option>Choose...</option>
-        <option>...</option>
-      </Form.Select>
-    </Form.Group>
-
-    <Form.Group as={Col} controlId="formGridZip">
-      <Form.Label>Zip</Form.Label>
-      <Form.Control />
-    </Form.Group>
-  </Row>
-
-  <Form.Group className="mb-3" id="formGridCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
-
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-</Form>
-
+        
             <div className='w-75 m-auto'>
                 <BootstrapTable 
                 keyField='fips_code' 
@@ -269,8 +225,7 @@ export default function QuizResults(){
                 pagination={paginationFactory(options)}
                 />
             </div>   
-
-            
+        
 
             <Modal show={show} onHide={handleClose}  >
                 <Modal.Header>
@@ -288,7 +243,29 @@ export default function QuizResults(){
                         </Row>
                        
                     </Container>
-                    <Container>{jobs && Jobs(jobs)}</Container>
+    <Container>
+        
+        {/*Advanced Filter */}
+        <Form>
+            <Row className="mb-2">
+        
+            <Col>
+                <Form.Group as={Col} controlId="formGridJob">
+                    <Form.Control onChange={KeywordChangeHandler} type="text" placeholder="Job Keyword (E.g. Data)" />
+                </Form.Group>
+            </Col>
+            
+            <Col>
+                <Button variant="secondary" type="submit">
+                    Submit
+                </Button>
+                </Col>
+                </Row>  
+    </Form>
+    {jobs && Jobs(jobs)}   
+         
+    </Container>
+    
       
                 </Modal.Body>
                 
